b321908ac8849c63e22eafb7d7844e5832570ff6
Rukiye Arabacı <rukiye.arbc6@gmail.com>
2024-01-26 01:59:09 +0300

    configureStore edited

--- src/store/configureStore.ts
+++ src/store/configureStore.ts
@@ -5,11 +5,9 @@ import { carReducer } from "./slices/carSlice";
 import { brandReducer } from "./slices/brandSlice";
 import { signInReducer } from './slices/signInSlice';
 import { showRentalReducer } from './slices/showRentalSlice';
-<<<<<<< HEAD
 import { carBodyTypeReducer } from './slices/carBodyTypeSlice';
-=======
 import { colorReducer } from './slices/colorSlice';
->>>>>>> 11a9be47ea687df678e4b2b9eb5515355a5d312f
+
 
 const rootReducer = combineReducers({
     car:carReducer,
@@ -18,11 +16,8 @@ const rootReducer = combineReducers({
     signIn:signInReducer,
     showRental:showRentalReducer,
     carModel:carModelReducer,
-<<<<<<< HEAD
-    carBodyType:carBodyTypeReducer
-=======
+    carBodyType:carBodyTypeReducer,
     color:colorReducer,
->>>>>>> 11a9be47ea687df678e4b2b9eb5515355a5d312f
 });
 
 export const store =configureStore({reducer:rootReducer});
